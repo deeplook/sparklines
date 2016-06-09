a85bc0564d149e7eecf1893c67cebf7b25ab214f
Dinu Gherman <gherman@darwin.in-berlin.de>
2016-06-09 11:27:50 +0200

    Added test_valid_number for uncluttering input numbers, plus minor changes

--- sparklines/__main__.py
+++ sparklines/__main__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-CLI Entry point for the program.
+CLI entry point for the program.
 """
 
 from __future__ import unicode_literals, print_function
@@ -17,16 +17,16 @@ try:
 except ImportError:
     pass
 
-if sys.version_info.major >= 3:
-    from sparklines.sparklines import sparklines, demo, __version__
-else:
-    from sparklines import sparklines, demo, __version__
-
 try:
     import termcolor
     HAVE_TERMCOLOR = True
 except ImportError:
     HAVE_TERMCOLOR = False
+
+if sys.version_info.major >= 3:
+    from sparklines.sparklines import sparklines, demo, __version__
+else:
+    from sparklines import sparklines, demo, __version__
 
 
 def _float_or_none(num_str):
@@ -39,21 +39,37 @@ def _float_or_none(num_str):
     return res
 
 
-def is_valid_emphasis(arg):
-    if re.match('\w+\:(eq|gt|ge|lt|le)\:.+', arg):
+def test_valid_number(arg):
+    "Argparse validator for input numbers, basically floats or null/none."
+    # https://stackoverflow.com/questions/385558/extract-float-double-value
+
+    # ok if we find (can parse) a float, returning the respective substring
+    float_pat = r'[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?'
+    m = re.search(float_pat, arg)
+    if m:
+        return m.group(0)
+
+    # ok if we find a 'null' or 'none' string which is then returned
+    m = re.search('(null|none)', arg.lower())
+    if m:
+        return m.group(0)
+
+    # otherwise not ok, making argparse barf
+    raise ValueError()
+
+
+def test_valid_emphasis(arg):
+    "Argparse validator for color filter expressions."
+
+    pat = '\w+\:(eq|gt|ge|lt|le)\:.+'
+    if re.match(pat, arg):
         return arg
     else:
-        return False
+        raise ValueError()
 
 
 def main():
-    desc = 'Sparklines on the command-line, '
-    pi = [3, 1, 4, 5, 9, 2, 6]
-    if HAVE_TERMCOLOR:
-        line = sparklines(pi, emph=['red:ge:5'])[0]
-    else:
-        line = sparklines(pi)[0]
-    desc += 'e.g. %s for %s.' % (line, pi)
+    desc = "Sparklines on the command-line, e.g. ▃▁▄▁▄█▂▅ for 3 1 4 1 5 9 2 6."
     p = argparse.ArgumentParser(description=desc)
 
     p.add_argument('-v', '--verbose', action='store_true',
@@ -65,23 +81,24 @@ def main():
     p.add_argument('-d', '--demo', action='store_true',
         help='Show a few usage examples for given (mandatory) input values.')
 
-    help_emph = 'Emphasise values below or above a certain threshold (e.g. '
-    help_emph += '"green:gt:5.0"). Works only when optional dependancy ' 
-    help_emph += '"termcolor" is met (which is %s here). ' % HAVE_TERMCOLOR
-    help_emph += 'Otherwise has no effect.'
+    help_emph = '''Emphasise input values below or above a certain
+        threshold (e.g. "green:gt:5.0"). Works only when optional
+        dependancy "termcolor" is met (which is %s here).
+        Otherwise has no effect.''' % HAVE_TERMCOLOR
     p.add_argument('-e', '--emphasise', metavar='STRING',
-        type=is_valid_emphasis, nargs='+',
+        type=test_valid_emphasis, nargs='+',
         help=help_emph)
 
-    help_n = 'The number of lines for one sparkline (higher numbers increase '
-    help_n += 'the resolution). An integer >= 1 (default: 1).'
+    help_n = '''The number of lines for one sparkline (higher numbers
+        increase the resolution). An integer >= 1 (default: 1).'''
     p.add_argument('-n', '--num-lines', metavar='NUMBER',
         help=help_n, default='1', type=int)
 
-    help_nums = 'A positive numeric value >= 0, e.g. 0, 3.14, 2e2 or '
-    help_nums += 'null/None... Negative numbers work, too, but will give '
-    help_nums += 'unexpected results and raise a warning.'
-    p.add_argument('nums', metavar='VALUE',
+    help_nums = '''A positive numeric value >= 0, e.g. 0, 3.14, 2e2.
+        Negative numbers work, too, but will give unexpected results
+        and raise a warning. The string values null and None (in any
+        spelling) represent empty slots, but not the value 0!'''
+    p.add_argument('nums', metavar='VALUE', type=test_valid_number,
         help=help_nums, nargs='*', default=sys.stdin)
 
     a = args = p.parse_args()
